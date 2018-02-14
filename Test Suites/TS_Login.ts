edcbfc70e3f4bd0e42249dbc16e8ab504ce3d44a
Aekachai <sniperaek@gmail.com>
2018-02-14 11:50:19 +0700

    first commit

---
+++ Test Suites/TS_Login.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-02-01T11:07:36</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9c54801c-d57d-404c-be15-6c167ec3650a</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f288a47-3806-467b-bd96-a4e1c39e82e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/iOSwithKobiton</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
